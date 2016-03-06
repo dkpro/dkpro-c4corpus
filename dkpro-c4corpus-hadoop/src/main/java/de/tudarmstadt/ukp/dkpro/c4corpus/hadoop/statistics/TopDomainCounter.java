bbf4004254def5445f5c261d276ae5e696fbe184
habernal <habernal@users.noreply.github.com>
2016-03-06 16:58:20 +0100

    No issue. Top domain statistics.

--- dkpro-c4corpus-hadoop/src/main/java/de/tudarmstadt/ukp/dkpro/c4corpus/hadoop/statistics/TopDomainCounter.java
+++ dkpro-c4corpus-hadoop/src/main/java/de/tudarmstadt/ukp/dkpro/c4corpus/hadoop/statistics/TopDomainCounter.java
@@ -20,8 +20,8 @@ package de.tudarmstadt.ukp.dkpro.c4corpus.hadoop.statistics;
 
 import com.google.common.net.InternetDomainName;
 import de.tudarmstadt.ukp.dkpro.c4corpus.hadoop.io.WARCInputFormat;
+import de.tudarmstadt.ukp.dkpro.c4corpus.hadoop.io.WARCWritable;
 import de.tudarmstadt.ukp.dkpro.c4corpus.hadoop.statistics.helper.TextLongCountingReducer;
-import de.tudarmstadt.ukp.dkpro.c4corpus.warc.io.WARCRecord;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -96,25 +96,45 @@ public class TopDomainCounter
      * and omits the domain and count
      */
     public static class DomainMapper
-            extends Mapper<LongWritable, WARCRecord, Text, LongWritable>
+            extends Mapper<LongWritable, WARCWritable, Text, LongWritable>
     {
         private static final LongWritable ONE = new LongWritable(1L);
 
         @Override
-        protected void map(LongWritable key, WARCRecord value, Context context)
+        protected void map(LongWritable key, WARCWritable value, Context context)
                 throws IOException, InterruptedException
         {
+            String domain = null;
+            String host = null;
+
             try {
                 // extract the top domain
-                URI uri = new URI(value.getHeader().getTargetURI());
-                String domain = InternetDomainName.from(uri.getHost()).topPrivateDomain()
-                        .toString();
-
-                context.write(new Text(domain), ONE);
+                URI uri = new URI(value.getRecord().getHeader().getTargetURI());
+                host = uri.getHost();
             }
             catch (URISyntaxException e) {
                 // ignore entry
             }
+
+            if (host != null) {
+                try {
+                    domain = InternetDomainName.from(host).topPrivateDomain().toString();
+                }
+                catch (IllegalStateException ex) {
+                    // this fails for some cases which are valid, e.g.
+                    // Error: java.lang.IllegalStateException: Not under a public suffix: freedom.press
+                    domain = host;
+                }
+                catch (IllegalArgumentException ex) {
+                    // Error: java.lang.IllegalArgumentException: Not a valid domain name: '78.20.42.79'
+                    // ignore
+                }
+            }
+
+            if (domain != null) {
+                context.write(new Text(domain), ONE);
+            }
+
         }
     }
 }
